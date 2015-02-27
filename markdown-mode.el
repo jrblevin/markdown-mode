a9a56014855109b21f748f750cd3ec4bf926218d
Tom May <tom@tommay.net>
2015-02-26 17:41:27 -0800

    Respect font-lock-mode nil.
    font-lock-refresh-defaults can set font-lock-mode to t even
    if global-font-lock-mode and font-lock-global-modes are nil.
    So save the original value of font-lock-mode and set it back
    to nil if it was nil to begin with.

--- markdown-mode.el
+++ markdown-mode.el
@@ -5730,7 +5730,15 @@ This is an exact copy of `line-number-at-pos' for use in emacs21."
           '(markdown-mode-font-lock-keywords
             nil nil nil nil
             (font-lock-syntactic-face-function . markdown-syntactic-face)))
-    (when (fboundp 'font-lock-refresh-defaults) (font-lock-refresh-defaults))))
+    (when (fboundp 'font-lock-refresh-defaults)
+      ;; font-lock-refresh-defaults can set font-lock-mode to t even
+      ;; if global-font-lock-mode and font-lock-global-modes are nil.
+      ;; So save the original value of font-lock-mode and set it back
+      ;; to nil if it was nil to begin with.
+      (let ((save-font-lock-mode font-lock-mode))
+        (font-lock-refresh-defaults)
+        (when (not save-font-lock-mode)
+          (font-lock-mode -1))))))
 
 (defun markdown-enable-math (&optional arg)
   "Toggle support for inline and display LaTeX math expressions.
