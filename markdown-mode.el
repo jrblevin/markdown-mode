jrblevin/markdown-mode

commit 8300a052b2744bbd904c9632dd83d140064f1550

Ensure markdown-reload-extensions only runs once
If enable-local-variables is non-nil, then run add it as a
hack-local-variables-hook.  Otherwise, run it as markdown-mode is
loading.

markdown-mode.el
@@ -4795,10 +4795,14 @@ if ARG is omitted or nil."
   (set (make-local-variable 'font-lock-multiline) t)
   (set (make-local-variable 'font-lock-support-mode)
        markdown-font-lock-support-mode)
-  (markdown-reload-extensions)
   ;; Extensions
   (make-local-variable 'markdown-enable-math)
-  (add-hook 'hack-local-variables-hook 'markdown-reload-extensions)
+  ;; Reload extensions
+  (if (and enable-local-variables buffer-file-name)
+      ;; Add a buffer-local hook to reload after file-local variables are read
+      (add-hook 'hack-local-variables-hook 'markdown-reload-extensions nil t)
+    ;; File local variables disabled or not visiting a file, reload now
+    (markdown-reload-extensions))
   ;; For imenu support
   (setq imenu-create-index-function 'markdown-imenu-create-index)
   ;; For menu support in XEmacs

commit 86fb0ea4685afc28a109a7702871a30c86fe81a0

Ensure font-lock-defaults is always populated on load
Attempt to fix an issue introduced in commit 8300a05.

The behavior now is to always run `markdown-reload-extensions` when
initially loading `markdown-mode'.  Later, when `hack-local-variables`
runs the new `markdown-handle-local-variables' hook function, we only
reload again if there are actually `markdown-mode`-related local
variables that might require different `font-lock-defaults`.

markdown-mode.el
@@ -4821,6 +4821,14 @@ if ARG is omitted or nil."
     (message "markdown-mode math support disabled"))
   (markdown-reload-extensions))
 
+(defun markdown-handle-local-variables ()
+  "Runs as a `hack-local-variables-hook' to update font lock rules.
+Checks to see if there is actually a markdown-mode file local variable
+before regenerating font-lock rules for extensions."
+  (when (and (boundp 'file-local-variables-alist)
+             (assoc 'markdown-enable-math file-local-variables-alist))
+    (markdown-reload-extensions)))
+
 
 ;;; Mode Definition  ==========================================================
 
@@ -4853,11 +4861,9 @@ if ARG is omitted or nil."
   ;; Extensions
   (make-local-variable 'markdown-enable-math)
   ;; Reload extensions
-  (if (and enable-local-variables buffer-file-name)
-      ;; Add a buffer-local hook to reload after file-local variables are read
-      (add-hook 'hack-local-variables-hook 'markdown-reload-extensions nil t)
-    ;; File local variables disabled or not visiting a file, reload now
-    (markdown-reload-extensions))
+  (markdown-reload-extensions)
+  ;; Add a buffer-local hook to reload after file-local variables are read
+  (add-hook 'hack-local-variables-hook 'markdown-handle-local-variables nil t)
   ;; For imenu support
   (setq imenu-create-index-function 'markdown-imenu-create-index)
   ;; For menu support in XEmacs