8a3c9a59e32d7cf4a3c9d0354756cd280c0c7030
Syohei YOSHIDA <syohex@gmail.com>
2016-01-21 22:26:24 +0900

    Fix language collect script
    - Original code does not return language list. It returns value of
      kill-buffer
    - Original code ignores language which contains space like 'Emacs Lisp'
    - Replace space with '-' like 'Emacs Lisp' -> 'Emacs-Lisp'

--- scripts/get-recognized-gfm-languages.el
+++ scripts/get-recognized-gfm-languages.el
@@ -4,10 +4,11 @@
        (url-retrieve-synchronously
         "https://raw.githubusercontent.com/github/linguist/master/lib/linguist/languages.yml"
         t)))
-  (with-current-buffer urlbuf
-    (goto-char (point-min))
-    ;; get past http headers
-    (re-search-forward "^$")
-    (cl-loop while (re-search-forward "^\\([^#[:space:]]+?\\):" nil t)
-             collect (match-string-no-properties 1)))
-  (kill-buffer urlbuf))
+  (prog1 (with-current-buffer urlbuf
+           (goto-char (point-min))
+           ;; get past http headers
+           (re-search-forward "^$")
+           (cl-loop while (re-search-forward "^\\([^#[:space:]][^:]+\\):" nil t)
+                    for lang = (match-string-no-properties 1)
+                    collect (replace-regexp-in-string " " "-" lang)))
+    (kill-buffer urlbuf)))
