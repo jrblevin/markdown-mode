787ee2b9a7b1291623a6b5b26feb9a27f5fb5f52
Sebastian Wiesner <swiesner@lunaryorn.com>
2016-02-03 13:02:57 +0100

    Simplify title check

--- markdown-mode.el
+++ markdown-mode.el
@@ -2909,7 +2909,7 @@ be used to populate the title attribute when converted to XHTML."
         (insert url)
       ;; When no URL is given, leave cursor at END following the colon
       (setq end (point)))
-    (when (and title (> (length title) 0))
+    (when (> (length title) 0)
       (insert " \"" title "\""))
     (unless (looking-at "\n")
       (insert "\n"))
