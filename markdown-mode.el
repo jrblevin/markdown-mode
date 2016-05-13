c560ddd51b6066aeea47c9f1cd2f54474745ed10
Jason Blevins <jrblevin@sdf.org>
2016-05-13 09:18:35 -0400

    Fix wrong file name in source file

--- markdown-mode.el
+++ markdown-mode.el
@@ -114,7 +114,7 @@
 ;;     (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
 ;;     (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
 ;;
-;;     (autoload 'gfm-mode "gfm-mode"
+;;     (autoload 'gfm-mode "markdown-mode"
 ;;        "Major mode for editing GitHub Flavored Markdown files" t)
 ;;     (add-to-list 'auto-mode-alist '("README\\.md\\'" . gfm-mode))
 
