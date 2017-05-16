jrblevin/markdown-mode

commit d58718085dab45e5b8a80daa5643f94d917a383a

Fix markdown-header-face inherit from nil error
When `markdown-header-scaling` is nil, the :inherit attribute value of
`markdown-header-face` will be `(nil font-lock-function-name-faces)`.
When exporting html from org-mode file which contains markdown source
block, it will lead to an "Invalid face" error.

markdown-mode.el
@@ -2165,8 +2165,8 @@ Used when `markdown-header-scaling' is non-nil."
 (defun markdown-make-header-faces ()
   "Build the faces used for Markdown headers."
   (defface markdown-header-face
-    `((t (:inherit (,(when markdown-header-scaling 'variable-pitch)
-                     font-lock-function-name-face)
+    `((t (:inherit (,@(when markdown-header-scaling 'variable-pitch)
+                    font-lock-function-name-face)
                    :weight bold)))
     "Base face for headers."
     :group 'markdown-faces)

commit d07621b825d98db4518141da526174fc08f51b85

Better markdown-header-face fix
Fixes a bug introduced in d587180.

Closes #193.

markdown-mode.el
@@ -2183,12 +2183,13 @@ Used when `markdown-header-scaling' is non-nil."
 
 (defun markdown-make-header-faces ()
   "Build the faces used for Markdown headers."
-  (defface markdown-header-face
-    `((t (:inherit (,@(when markdown-header-scaling 'variable-pitch)
-                    font-lock-function-name-face)
-                   :weight bold)))
-    "Base face for headers."
-    :group 'markdown-faces)
+  (let ((inherit-faces '(font-lock-function-name-face)))
+    (when markdown-header-scaling
+      (setq inherit-faces (cons 'variable-pitch inherit-faces)))
+    (defface markdown-header-face
+      `((t (:inherit ,inherit-faces :weight bold)))
+      "Base face for headers."
+      :group 'markdown-faces))
   (dotimes (num 6)
     (let* ((num1 (1+ num))
            (face-name (intern (format "markdown-header-face-%s" num1)))