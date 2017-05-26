b5dcb8931bc313ec9af001fe55f1d47b5120cb69
Jason Blevins <jrblevin@sdf.org>
2017-05-26 09:18:51 -0400

    Add badges to homepage

--- webpage.sh
+++ webpage.sh
@@ -7,10 +7,18 @@ DATE=$(date +"%B %e, %Y %H:%M %Z")
 echo "title: Markdown Mode for Emacs
 description: A major mode for GNU Emacs for editing Markdown-formatted text files.
 markup: markdown
-icon: emacs
 city: Columbus
 created: May 24, 2007 23:47 GMT
-modified: $DATE" > index.text
+modified: $DATE
+style: #badges { margin-bottom: 1.5rem; }
+
+<div id=\"badges\">
+<a href=\"https://github.com/jrblevin/markdown-mode\"><img src=\"https://img.shields.io/github/stars/jrblevin/markdown-mode.svg?style=social&label=GitHub\" alt=\"GitHub\"/></a>
+<a href=\"https://melpa.org/#/markdown-mode\"><img src=\"https://melpa.org/packages/markdown-mode-badge.svg\" alt=\"MELPA badge\"/></a>
+<a href=\"https://stable.melpa.org/#/markdown-mode\"><img src=\"https://stable.melpa.org/packages/markdown-mode-badge.svg\" alt=\"MELPA stable badge\"/></a>
+<a href=\"https://travis-ci.org/jrblevin/markdown-mode\"><img src=\"https://travis-ci.org/jrblevin/markdown-mode.svg?branch=master\" alt=\"Travis CI Build Status\"/></a>
+<a href=\"https://leanpub.com/markdown-mode\"><img src=\"https://img.shields.io/badge/leanpub-guide-orange.svg\" alt=\"Guide to Markdown Mode for Emacs\"/></a>
+</div>" > index.text
 
 echo "# Emacs Markdown Mode [![MELPA badge][melpa-badge]][melpa-link] [![MELPA stable badge][melpa-stable-badge]][melpa-stable-link] [![Travis CI Build Status][travis-badge]][travis-link] [![Guide to Markdown Mode for Emacs][leanpub-badge]][leanpub-link]
 
