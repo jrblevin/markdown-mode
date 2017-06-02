f172a5bf77a17cd25ccdc2026299fc30fe635947
Jason Blevins <jrblevin@sdf.org>
2017-06-02 12:57:02 -0400

    Rename button type

--- markdown-mode.el
+++ markdown-mode.el
@@ -886,6 +886,7 @@
 (require 'thingatpt)
 (require 'cl-lib)
 (require 'url-parse)
+(require 'button)
 
 (defvar jit-lock-start)
 (defvar jit-lock-end)
@@ -5329,7 +5330,7 @@ BUFFER-NAME is the name of the main buffer being visited."
 
 ;; Jumps to a particular link at location given by 'target-char
 ;; property in buffer given by 'target-buffer property.
-(define-button-type 'markdown-link-button
+(define-button-type 'markdown-location-button
   'help-echo "mouse-1, RET: jump to location of link"
   'follow-link t
   'face 'bold
@@ -5372,7 +5373,7 @@ the link text, location, and line number."
         (line (cl-third link)))
     ;; Create a reference button
     (insert-button label
-                   :type 'markdown-link-button
+                   :type 'markdown-location-button
                    'target-buffer oldbuf
                    'target-char char)
     (insert (format " (line %d)\n" line))))
@@ -7182,8 +7183,6 @@ if ARG is omitted or nil."
 
 
 ;;; GFM Checkboxes ============================================================
-
-(require 'button)
 
 (define-button-type 'markdown-gfm-checkbox-button
   'follow-link t
