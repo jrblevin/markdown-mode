08106dec81bdf5eebad9b40899743cf63dc70124
Jason Blevins <jrblevin@sdf.org>
2017-06-05 14:12:53 -0400

    Only set sentence-end-double-space for elisp

--- .dir-locals.el
+++ .dir-locals.el
@@ -5,12 +5,12 @@
   (require-final-newline . t)
   ;; not tabs in code
   (indent-tabs-mode)
-  ;; checkdoc, one space is enough
-  (sentence-end-double-space . nil)
   ;; checkdoc, don't botch English grammar
   (checkdoc-arguments-in-order-flag . nil)
   ;; checkdoc, we don't want docs for internal vars
   (checkdoc-force-docstrings-flag . nil))
  (emacs-lisp-mode
+  ;; checkdoc, one space is enough
+  (sentence-end-double-space . nil)
   ;; remove trailing whitespace
   (eval . (add-hook 'before-save-hook 'delete-trailing-whitespace nil t))))
