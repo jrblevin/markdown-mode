jrblevin/markdown-mode

commit 66566a550dfdf30f8b8e8522d196dd00cfc9ae88

Fixed fill-paragraph so that it does not break line inside of square brackets (maruku does not like it when you do that)

markdown-mode.el
@@ -1368,6 +1368,12 @@ This is an exact copy of `line-number-at-pos' for use in emacs21."
       (forward-line 0)
       (1+ (count-lines start (point))))))
 
+(defun markdown-nobreak-p ()
+  "Returns nil if it is ok for fill-paragraph to insert a line
+  break at point"
+  ;; are we inside in square brackets
+  (looking-back "\\[[^]]*"))
+
 
 
 ;;; Mode definition  ==========================================================
@@ -1406,6 +1412,14 @@ This is an exact copy of `line-number-at-pos' for use in emacs21."
   (setq indent-line-function markdown-indent-function))
 
 ;(add-to-list 'auto-mode-alist '("\\.text$" . markdown-mode))
+
+
+(add-hook 'markdown-mode-hook
+	  (lambda ()
+	    (make-local-variable 'fill-nobreak-predicate)
+	    (add-hook 'fill-nobreak-predicate 'markdown-nobreak-p)))
+
+
 
 (provide 'markdown-mode)
 

commit 3221449860405a51dddb546a09249ee2410a034d

Stop inhibiting line breaks inside links when filling
This fill-nobreak-predicate hook was introduced in 66566a5 as a
workaround for Maruku, which at the time did not correctly parse links
with internal line breaks.  That has since been fixed.

Also update the corresponding test case to properly set fill-column.

Closes GH-173.

markdown-mode.el
@@ -8862,8 +8862,6 @@ position."
   ;; Separating out each condition into a separate function so that users can
   ;; override if desired (with remove-hook)
   (add-hook 'fill-nobreak-predicate
-            #'markdown-inside-link-p nil t)
-  (add-hook 'fill-nobreak-predicate
             #'markdown-line-is-reference-definition-p nil t)
   (add-hook 'fill-nobreak-predicate
             #'markdown-pipe-at-bol-p nil t)

tests/markdown-test.el
@@ -4721,11 +4721,14 @@ paragraph 2")))))
       (should (string-equal (buffer-string) text)))))
 
 (ert-deftest test-markdown-filling/long-paragraph-with-link ()
-  "Test `fill-paragraph' on a long paragraph with a long link."
+  "Test `fill-paragraph' on a long paragraph with a long link.
+See GH-173."
   (markdown-test-string
    "aaa aaa aaa aaa aaa aaa aaa aaa aaa aaa aaa aaa aaa aaa aaa aaa aaa aaa aaa aaa aaa aaa aaa aaa aaa aaa aaa aaa aaa aaa aaa aaa aaa aaa aaa aaa [aaa aaa aaa aaa](aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa) aaa aaa aaa aaa aaa."
-   (fill-paragraph)
-   (should (string-equal (buffer-string) "aaa aaa aaa aaa aaa aaa aaa aaa aaa aaa aaa aaa aaa aaa aaa aaa aaa\naaa aaa aaa aaa aaa aaa aaa aaa aaa aaa aaa aaa aaa aaa aaa aaa aaa\naaa aaa [aaa aaa aaa aaa](aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa) aaa\naaa aaa aaa aaa."))))
+   (let ((fill-column 79)) (fill-paragraph))
+   (should (string-equal (buffer-string) "aaa aaa aaa aaa aaa aaa aaa aaa aaa aaa aaa aaa aaa aaa aaa aaa aaa aaa aaa aaa
+aaa aaa aaa aaa aaa aaa aaa aaa aaa aaa aaa aaa aaa aaa aaa aaa [aaa aaa aaa
+aaa](aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa) aaa aaa aaa aaa aaa."))))
 
 (ert-deftest test-markdown-filling/pandoc-line-blocks ()
   "Filling should leave Pandoc line blocks undisturbed.