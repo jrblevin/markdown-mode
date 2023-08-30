193b61605f44c85d261b8bd82e0a213fd8f1ff32
Shohei YOSHIDA <syohex@gmail.com>
2023-08-30 22:35:12 +0900

    Update version for v2.6 release

--- markdown-mode.el
+++ markdown-mode.el
@@ -1,12 +1,12 @@
 ;;; markdown-mode.el --- Major mode for Markdown-formatted text -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2007-2022 Jason R. Blevins and markdown-mode
+;; Copyright (C) 2007-2023 Jason R. Blevins and markdown-mode
 ;; contributors (see the commit log for details).
 
 ;; Author: Jason R. Blevins <jblevins@xbeta.org>
 ;; Maintainer: Jason R. Blevins <jblevins@xbeta.org>
 ;; Created: May 24, 2007
-;; Version: 2.6-alpha
+;; Version: 2.6
 ;; Package-Requires: ((emacs "27.1"))
 ;; Keywords: Markdown, GitHub Flavored Markdown, itex
 ;; URL: https://jblevins.org/projects/markdown-mode/
@@ -55,7 +55,7 @@
 
 ;;; Constants =================================================================
 
-(defconst markdown-mode-version "2.6-alpha"
+(defconst markdown-mode-version "2.6"
   "Markdown mode version number.")
 
 (defconst markdown-output-buffer-name "*markdown-output*"
